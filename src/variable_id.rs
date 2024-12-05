1d32c39269002c86cb06bbcaf31efbca2e73481a
Samuel Pastva <sam.pastva@gmail.com>
2024-12-05 12:11:43 +0100

    Minor refactor of `VarIdPacked32`

--- src/variable_id.rs
+++ src/variable_id.rs
@@ -3,7 +3,7 @@ use std::hash::Hash;
 /// An internal trait implemented by types that can serve as BDD variable identifiers.
 /// The core feature of this trait is that a variable ID must have one designated
 /// "undefined" value (similar to `Option::None`). Furthermore, it must hold that
-/// `id <= undefined` for all values `id` of the type implementing the [VariableId] trait.
+/// `id < undefined` for every other `id` value of the same type.
 pub trait VariableId: PartialEq + Eq + PartialOrd + Ord + Hash {
     fn undefined() -> Self;
     fn is_undefined(&self) -> bool;
@@ -13,21 +13,33 @@ pub trait VariableId: PartialEq + Eq + PartialOrd + Ord + Hash {
 /// information about the node containing the variable into the variable ID
 /// to make the apply algorithm more efficient.
 ///
-/// This means that [VarIdPacked32] can only represent 2**29 - 1 unique variables.
+/// This means that [VarIdPacked32] can only represent `2**29 - 1` unique variables.
 ///
 /// The packed information is as follows:
-/// Two low bits are used as a {0, 1, many} counter that keeps track of how many
-/// parents the node containing the variable has.
-/// Third low bit is used to indicate if the node containing the variable should
-/// use the task cache in the apply algorithm.
+///  - Two least-significant bits are used as a `{0, 1, many}` counter that keeps track of how
+///    many parents the node containing the variable has.
+///  - Third least-significant bit is used to indicate if the node containing the variable should
+///    use the task cache in the apply algorithm.
 #[derive(Clone, Copy, Debug)]
 pub struct VarIdPacked32(u32);
 
+/// An internal "mask bit" that is used when manipulating the "use cache"
+/// flag in packed variable IDs.
+const USE_CACHE_MASK: u32 = 0b100;
+
 impl VarIdPacked32 {
-    /// Create a new instance of [VarIdPacked32] with the specified variable ID.
+    /// Create a new instance of [VarIdPacked32] with the specified variable ID
+    /// (that is not *undefined*).
+    ///
     /// The variable ID is shifted left by 3 bits to make room for the additional
     /// information.
     pub fn new(id: u32) -> VarIdPacked32 {
+        // `<<` should fail when overflowing in debug mode, but this is a bit easier to trace.
+        debug_assert!(
+            id < Self::undefined().unpack(),
+            "Variable ID too large: {}",
+            id
+        );
         VarIdPacked32(id << 3)
     }
 
@@ -42,11 +54,11 @@ impl VarIdPacked32 {
     }
 
     pub(crate) fn use_cache(&self) -> bool {
-        self.0 & 0b100 != 0
+        self.0 & USE_CACHE_MASK != 0
     }
 
     pub(crate) fn set_use_cache(&mut self, value: bool) {
-        self.0 = (self.0 & !(1 << 2)) | (u32::from(value) << 2);
+        self.0 = (self.0 & !USE_CACHE_MASK) | (u32::from(value) << 2);
     }
 
     pub(crate) fn increment_parents(&mut self) {
