cb97c6f95abf5c0d0a08eb28c7c261cc3d741ff1
Samuel Pastva <sam.pastva@gmail.com>
2024-12-06 19:10:59 +0100

    Some documentation/tests for three-valued logic, plus a minor bugfix.

--- src/boolean_operators.rs
+++ src/boolean_operators.rs
@@ -1,7 +1,11 @@
+//! Defines three-valued logic operators based on [TriBool]. These are used to implement
+//! Boolean operators in the `apply` algorithms.
+//!
 use std::cmp::{max, min};
 
 use crate::node_id::BddNodeId;
 
+/// Applies a three-value logic operator to a pair of [BddNodeId] arguments.
 pub fn lift_operator<NodeId, TriBoolOperator>(
     left: NodeId,
     right: NodeId,
@@ -16,6 +20,7 @@ where
     BddNodeId::from_three_valued(operator(left, right))
 }
 
+/// Thee-valued conjunction of two [BddNodeId] identifiers.
 pub fn and<NodeId>(left: NodeId, right: NodeId) -> NodeId
 where
     NodeId: BddNodeId,
@@ -23,6 +28,7 @@ where
     lift_operator(left, right, TriBool::and)
 }
 
+/// Thee-valued disjunction of two [BddNodeId] identifiers.
 pub fn or<NodeId>(left: NodeId, right: NodeId) -> NodeId
 where
     NodeId: BddNodeId,
@@ -30,6 +36,7 @@ where
     lift_operator(left, right, TriBool::or)
 }
 
+/// Thee-valued exclusive or (non-equivalence) of two [BddNodeId] identifiers.
 pub fn xor<NodeId>(left: NodeId, right: NodeId) -> NodeId
 where
     NodeId: BddNodeId,
@@ -37,6 +44,7 @@ where
     lift_operator(left, right, TriBool::xor)
 }
 
+/// Thee-valued implication of two [BddNodeId] identifiers.
 pub fn implies<NodeId>(left: NodeId, right: NodeId) -> NodeId
 where
     NodeId: BddNodeId,
@@ -44,6 +52,7 @@ where
     lift_operator(left, right, TriBool::implies)
 }
 
+/// Thee-valued equivalence of two [BddNodeId] identifiers.
 pub fn iff<NodeId>(left: NodeId, right: NodeId) -> NodeId
 where
     NodeId: BddNodeId,
@@ -61,14 +70,17 @@ pub enum TriBool {
 }
 
 impl TriBool {
+    /// Logical disjunction.
     pub(crate) fn or(self, other: Self) -> Self {
         max(self, other)
     }
 
+    /// Logical conjunction.
     pub(crate) fn and(self, other: Self) -> Self {
         min(self, other)
     }
 
+    /// Exclusive or (non-equivalence).
     pub(crate) fn xor(self, other: Self) -> Self {
         // min(max(a,b), neg(min(a,b)))
         let [smaller, greater] = if self < other {
@@ -79,12 +91,14 @@ impl TriBool {
         min(greater, !smaller)
     }
 
+    /// Implication.
     pub(crate) fn implies(self, other: Self) -> Self {
-        !self.or(!other)
+        (!self).or(other)
     }
 
+    /// Equivalence.
     pub(crate) fn iff(self, other: Self) -> Self {
-        !(self.xor(other))
+        !self.xor(other)
     }
 }
 
@@ -97,5 +111,41 @@ impl std::ops::Not for TriBool {
             Self::Indeterminate => Self::Indeterminate,
             Self::False => Self::True,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::node_id::NodeId32;
+
+    #[test]
+    pub fn three_valued_logic_invariants() {
+        // Just a representative subset of input-output pairs for each operator.
+
+        assert!(and(NodeId32::one(), NodeId32::one()).is_one());
+        assert!(and(NodeId32::zero(), NodeId32::one()).is_zero());
+        assert!(and(NodeId32::zero(), NodeId32::undefined()).is_zero());
+        assert!(and(NodeId32::one(), NodeId32::undefined()).is_undefined());
+
+        assert!(or(NodeId32::zero(), NodeId32::zero()).is_zero());
+        assert!(or(NodeId32::one(), NodeId32::zero()).is_one());
+        assert!(or(NodeId32::one(), NodeId32::undefined()).is_one());
+        assert!(or(NodeId32::zero(), NodeId32::undefined()).is_undefined());
+
+        assert!(implies(NodeId32::one(), NodeId32::zero()).is_zero());
+        assert!(implies(NodeId32::zero(), NodeId32::zero()).is_one());
+        assert!(implies(NodeId32::zero(), NodeId32::undefined()).is_one());
+        assert!(implies(NodeId32::one(), NodeId32::undefined()).is_undefined());
+
+        assert!(xor(NodeId32::one(), NodeId32::one()).is_zero());
+        assert!(xor(NodeId32::zero(), NodeId32::one()).is_one());
+        assert!(xor(NodeId32::zero(), NodeId32::undefined()).is_undefined());
+        assert!(xor(NodeId32::one(), NodeId32::undefined()).is_undefined());
+
+        assert!(iff(NodeId32::one(), NodeId32::one()).is_one());
+        assert!(iff(NodeId32::zero(), NodeId32::one()).is_zero());
+        assert!(iff(NodeId32::zero(), NodeId32::undefined()).is_undefined());
+        assert!(iff(NodeId32::one(), NodeId32::undefined()).is_undefined());
     }
 }
